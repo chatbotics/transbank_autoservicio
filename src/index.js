e4d9ecbab831ef5ec4d530c3c9a3b1dc4413e0c9
finecta <aldo@powermindchile.cl>
2019-01-22 17:02:59 -0300

    Se cambia el parser para la data del voucher

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const SerialPort = require('serialport')
-const Regex = require('@serialport/parser-regex')
 const event = require('events')
 
 const emitter = new event.EventEmitter()
@@ -7,104 +6,118 @@ let port
 let serialport_opened
 
 
-function init(p, config){
+function init(p, config) {
     port = new SerialPort(p, config)
     serialport_opened = false
-    dataParser = port.pipe(new Regex({regex:UX300.ETX}))
     initListeners()
 }
 
-function initListeners(){
-    dataParser.on('data', function(data) {
-        if(data[0] != 6) {
-            data = data.toString('utf-8').substring(2, data.toString('utf-8').length)
-            parseTransaction(data.split('|'))
+function initListeners() {
+    let resultVoucher = Buffer.alloc(0)
+    port.on('data', function (data) {
+        if(data[0] == 6) {
+            console.log("ES UN ACK!")
         }
-        
+        else if(data[0] == 2 && data[data.length-2] == 3) {
+            let result = data.slice(1, data.length-2).toString('utf-8')
+            parseTransaction(result.split('|'))
+        }
+        else {
+            resultVoucher = Buffer.concat([resultVoucher, data])
+            
+            for(let e of data){
+                if (e == 3){
+                    console.log('Terminoooooooooo')
+                    parseTransaction(resultVoucher.slice(1).toString('utf-8').split('|'))
+                    resultVoucher = Buffer.alloc(0)
+                    break;
+                }
+            }
+        }
     })
 
-    port.on('error', function(data) {
+    port.on('error', function (data) {
         console.log('Error: ' + data)
     })
-    
+
 }
 
 const UX300 = {
-    STX:                                '\x02',
-    BEL:                                '\x07',
-    SEP:                                '\x7c',
-    ETX:                                '\x03',
-    FS:                                 '\x1c',
-    ACK:                                '\x06',
-    NAK:                                '\x15',
-    
-    PAYMENT_REQUEST:                    '0200',
-    PAYMENT_RESPONSE:                   '0210',
+    STX: '\x02',
+    BEL: '\x07',
+    SEP: '\x7c',
+    ETX: '\x03',
+    FS: '\x1c',
+    ACK: '\x06',
+    NAK: '\x15',
 
-    LAST_PAYMENT_REQUEST:               '0250',
-    LAST_PAYMENT_RESPONSE:              '0260',
+    PAYMENT_REQUEST: '0200',
+    PAYMENT_RESPONSE: '0210',
 
-    CANCEL_TRANSACTION_REQUEST:         '1200',
-    CANCEL_TRANSACTION_RESPONSE:        '1210',
+    LAST_PAYMENT_REQUEST: '0250',
+    LAST_PAYMENT_RESPONSE: '0260',
 
-    CLOSE_TRANSACTIONS_REQUEST:         '0500',
-    CLOSE_TRANSACTIONS_RESPONSE:        '0510',
+    CANCEL_TRANSACTION_REQUEST: '1200',
+    CANCEL_TRANSACTION_RESPONSE: '1210',
 
-    LOAD_KEYS_REQUEST:                  '0800',
-    LOAD_KEYS_RESPONSE:                 '0810',
+    CLOSE_TRANSACTIONS_REQUEST: '0500',
+    CLOSE_TRANSACTIONS_RESPONSE: '0510',
 
-    POLLING_REQUEST:                    '0100',
-    
-    INITIALIZATION_REQUEST:             '0070',
-    
-    INITIALIZATION_RESPONSE_REQUEST:    '0080',
-    INITIALIZATION_RESPONSE_RESPONSE:   '1080',
+    LOAD_KEYS_REQUEST: '0800',
+    LOAD_KEYS_RESPONSE: '0810',
 
-    ENCRYPTED_PAN_REQUEST:              '0400',
-    ENCRYPTED_PAN_RESPONSE:             '0410',
+    POLLING_REQUEST: '0100',
 
-    INTERMEDIATE_MESSAGES:              '0900',
-    USE_CARD:                             '80',
-    WRITE_PIN:                            '81',
-    PROCESSING_PAYMENT:                   '82',
-    
-    REJECTED:                             '01',
-    APPROVED:                             '00',
-    TRANSBANK_NO_RESPONSE:                '02',
-    CONNECTION_FAIL:                      '03',
-    TRANSACTION_WAS_CANCELLED:            '04',
-    TRANSACTION_DOESNT_EXISTS:            '05',
-    UNSUPPORTED_CARD:                     '06',
-    CANCELLED_TRANSACTION:                '07',
-    CANT_CANCEL_TRANSACTION:              '08',
-    CARD_READ_ERROR:                      '09',
-    LOW_AMOUNT_ERROR:                     '10',
-    PAYMENT_DOESNT_EXISTS:                '11',
-    UNSUPPORTED_TRANSACTION:              '12',
-    MUST_EXECUTE_CLOSE:                   '13',
-    CRYPT_ERROR_PAN:                      '14',
-    DEBIT_OPERATION_ERROR:                '15',
+    INITIALIZATION_REQUEST: '0070',
 
-    INITIALIZATION_SUCCESS:               '90',
-    INITIALIZATION_FAIL:                  '91',
-    READER_DISCONNECTED:                  '92',
+    INITIALIZATION_RESPONSE_REQUEST: '0080',
+    INITIALIZATION_RESPONSE_RESPONSE: '1080',
 
-    calcLRC: function(command) {
+    ENCRYPTED_PAN_REQUEST: '0400',
+    ENCRYPTED_PAN_RESPONSE: '0410',
+
+    INTERMEDIATE_MESSAGES: '0900',
+    USE_CARD: '80',
+    WRITE_PIN: '81',
+    PROCESSING_PAYMENT: '82',
+
+    REJECTED: '01',
+    APPROVED: '00',
+    TRANSBANK_NO_RESPONSE: '02',
+    CONNECTION_FAIL: '03',
+    TRANSACTION_WAS_CANCELLED: '04',
+    TRANSACTION_DOESNT_EXISTS: '05',
+    UNSUPPORTED_CARD: '06',
+    CANCELLED_TRANSACTION: '07',
+    CANT_CANCEL_TRANSACTION: '08',
+    CARD_READ_ERROR: '09',
+    LOW_AMOUNT_ERROR: '10',
+    PAYMENT_DOESNT_EXISTS: '11',
+    UNSUPPORTED_TRANSACTION: '12',
+    MUST_EXECUTE_CLOSE: '13',
+    CRYPT_ERROR_PAN: '14',
+    DEBIT_OPERATION_ERROR: '15',
+
+    INITIALIZATION_SUCCESS: '90',
+    INITIALIZATION_FAIL: '91',
+    READER_DISCONNECTED: '92',
+
+    calcLRC: function (command) {
         command = command + UX300.ETX
         var buf = new ArrayBuffer(command.length)
         var bufView = new Uint8Array(buf)
-        for ( var i = 0; i < command.length; i++ ) {
+        for (var i = 0; i < command.length; i++) {
             bufView[i] = command.charCodeAt(i)
         }
 
         var lrc = 0
-        for ( var i = 0; i < command.length; i++ ) {
-            lrc = ( lrc ^ bufView[i] ) & 0xFF
-        } 
+        for (var i = 0; i < command.length; i++) {
+            lrc = (lrc ^ bufView[i]) & 0xFF
+        }
         return String.fromCharCode(lrc)
 
-    }, 
-    
+    },
+
     polling() {
         sendCommand(UX300.POLLING_REQUEST)
     },
@@ -138,17 +151,17 @@ const UX300 = {
 
     connect() {
         port.open(error => {
-            if ( error ) {
+            if (error) {
                 console.log('failed to open port: ' + error)
             } else {
                 console.log('serial port opened')
                 serialport_opened = true
-                
+
             }
         })
     },
 
-    bindOnOpen() {
+    onOpen() {
         return new Promise((resolve, reject) => {
             port.on('open', resolve)
         })
@@ -177,11 +190,11 @@ const UX300 = {
 }
 
 function sendCommand(data) {
-    if(serialport_opened) {
+    if (serialport_opened) {
         const LRC = UX300.calcLRC(data)
         const command = UX300.STX + data + UX300.ETX + LRC
-        port.write(command, function(err) {
-            if(err)
+        port.write(command, function (err) {
+            if (err)
                 console.log("Error:", err)
             else {
                 console.log("Command sended:", command)
@@ -193,95 +206,46 @@ function sendCommand(data) {
 }
 
 function parseTransaction(data) {
-    switch(data[0]) {
+    
+    let typeMessage = data[0]
+    let responseCode = data[1]
+    switch (typeMessage) {
         case UX300.INTERMEDIATE_MESSAGES:
-            switch(data[1]) {
-                case UX300.USE_CARD:
-                    emitter.emit('intermediate_messages','use_card')
-                    break;
-                case UX300.WRITE_PIN:
-                    emitter.emit('intermediate_messages', 'write_pin')
-                    break;
-                case UX300.PROCESSING_PAYMENT:
-                    emitter.emit('intermediate_messages','processing_payment')
-                    break;
-            }
-            break;
-        
-        case UX300.PAYMENT_RESPONSE:    
-            switch(data[1]) {
+            emitter.emit(UX300.INTERMEDIATE_MESSAGES, data[1])
+            break
+
+        case UX300.PAYMENT_RESPONSE:
+            switch (responseCode) {
                 case UX300.APPROVED:
-                    emitter.emit('payment_voucher', data[15].match(/.{1,40}/g))
+                    if (data[15] != undefined) {
+                        emitter.emit('payment_voucher', data[15].match(/.{1,40}/g))
+                        port.write(UX300.ACK)
+                    }
+                    else {
+                        emitter.emit('error', data[1])
+                        port.write(UX300.ACK)
+                    }
                     break;
-                case UX300.REJECTED:
-                    emitter.emit('error', UX300.REJECTED)
-                    break;
-                case UX300.TRANSBANK_NO_RESPONSE:
-                    emitter.emit('error', UX300.TRANSBANK_NO_RESPONSE)
-                    break;
-                case UX300.CONNECTION_FAIL:
-                    emitter.emit('error', UX300.CONNECTION_FAIL)
-                    break;
-                case UX300.TRANSACTION_WAS_CANCELLED:
-                    emitter.emit('error', UX300.TRANSACTION_WAS_CANCELLED)
-                    break;
-                case UX300.TRANSACTION_DOESNT_EXISTS:
-                    emitter.emit('error', UX300.TRANSACTION_DOESNT_EXISTS)
-                    break;
-                case UX300.UNSUPPORTED_CARD:
-                    emitter.emit('error', UX300.UNSUPPORTED_CARD)
-                    break;
-                case UX300.CANCELLED_TRANSACTION:
-                    emitter.emit('error', UX300.CANCELLED_TRANSACTION)
-                    break;
-                case UX300.CANT_CANCEL_TRANSACTION:
-                    emitter.emit('error', UX300.CANT_CANCEL_TRANSACTION)
-                    break;
-                case UX300.CARD_READ_ERROR:
-                    emitter.emit('error', UX300.CARD_READ_ERROR)
-                    break;
-                case UX300.LOW_AMOUNT_ERROR:
-                    emitter.emit('error', UX300.LOW_AMOUNT_ERROR)
-                    break;
-                case UX300.PAYMENT_DOESNT_EXISTS:
-                    emitter.emit('error', UX300.PAYMENT_DOESNT_EXISTS)
-                    break;
-                case UX300.UNSUPPORTED_TRANSACTION:
-                    emitter.emit('error', UX300.UNSUPPORTED_TRANSACTION)
-                    break;
-                case UX300.MUST_EXECUTE_CLOSE:
-                    emitter.emit('error', UX300.MUST_EXECUTE_CLOSE)
-                    break;
-                case UX300.CRYPT_ERROR_PAN:
-                    emitter.emit('error', UX300.CRYPT_ERROR_PAN)
-                    break;
-                case UX300.DEBIT_OPERATION_ERROR:
-                    emitter.emit('error', UX300.DEBIT_OPERATION_ERROR)
+                default:
+                    emitter.emit('error', responseCode)
+                    port.write(UX300.ACK)
                     break;
             }
-            if(data[15] != undefined) {
-                emitter.emit('payment_voucher', data[15].match(/.{1,40}/g))
-            }
-            port.write(UX300.ACK)
             break;
 
+        case UX300.CLOSE_TRANSACTIONS_RESPONSE:
+            if (responseCode == UX300.APPROVED) {
+                emitter.emit('transactions_close', data[4].match(/.{1,40}/g))
+                port.write(UX300.ACK)
+            }
+            break
         default:
-            console.log("Not recognized data:", data)
-        break;
+            console.log('default', typeMessage)
     }
 }
 
 module.exports = {
     init: init,
-    connect: UX300.connect,
-    onOpen: UX300.bindOnOpen,
-    flush: UX300.flush,
-    initialize: UX300.initialize,
-    loadKeys: UX300.loadKeys,
-    pay: UX300.pay,
-    lastTransaction: UX300.lastTransaction,
-    cancelTransaction: UX300.cancelTransaction,
-    closeTransactions: UX300.closeTransactions,
-    polling: UX300.polling,
-    events: emitter
+    events: emitter,
+    ...UX300
 }
